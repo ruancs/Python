7e0b49d26260de7b18c0d96fbc92a1de16427061
ruancs <ruan.cs@hotmail.com>
2020-02-10 02:12:48 -0300

    exercício resolvido

---
+++ adjacentes.py
@@ -0,0 +1,32 @@
+num = int(input("Digite um número inteiro: "))
+
+i = '0'
+
+x = 0
+
+j = '0'
+
+y = 0
+
+adj = False
+
+while num >=1 or adj == True:
+	i = (num % 10)
+	if (j == i):
+		adj = True
+		y = y + 1			
+		x = (num // 10)
+		num = x
+		j = i
+		
+	else:
+		adj = False
+		x = (num // 10)
+		num = x
+		j = i
+
+if y >= 1:
+	print("sim")
+else: 
+	print("não")
+
