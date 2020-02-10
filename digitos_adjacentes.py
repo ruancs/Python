d20316dabb876657f29203bbb51e7188dbca1d93
ruancs <ruan.cs@hotmail.com>
2020-02-10 01:59:09 -0300

    código consertado

--- digitos_adjacentes.py
+++ digitos_adjacentes.py
@@ -1,22 +1,26 @@
 num = int(input("Digite um número inteiro: "))
 
-i = 0
+i = ("0")
 
 x = 0
 
-j = 1
+j = ("0")
 
 y = 0
 
-while num >=1:
+adj = False
+
+while num >=1 or (adj == True):
 	i = (num % 10)
 	if (j == i):
-		y = y + 1
+		adj = True
+		y = y + 1			
 		x = (num // 10)
 		num = x
 		j = i
-
+	
 	else:
+		adj = False
 		x = (num // 10)
 		num = x
 		j = i
