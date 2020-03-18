8987cd4a31af903782f67afd611e6b8893cc770b
ruancs <ruan.cs@hotmail.com>
2020-03-18 02:01:29 -0300

    exercicio

---
+++ tabuada.py
@@ -0,0 +1,12 @@
+linha = 1
+coluna = 1
+
+while linha < 10:
+	print("**** Tabuada de",linha,"****")
+	while coluna <=10:
+		print((linha),"x", (coluna),"=",(linha*coluna))
+		coluna = coluna + 1
+	linha = linha + 1
+	print("********************")
+	coluna = 1
+
